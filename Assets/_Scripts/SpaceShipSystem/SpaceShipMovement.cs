842cbdb7a9391e76095a22aefe4251a2d8cbed9b
Emre Berat Kıran <87636497+EmreBeratKR@users.noreply.github.com>
2022-12-02 05:27:41 +0300

    Jittery movement fix

--- Assets/_Scripts/SpaceShipSystem/SpaceShipMovement.cs
+++ Assets/_Scripts/SpaceShipSystem/SpaceShipMovement.cs
@@ -14,6 +14,7 @@ namespace SpaceShipSystem
         [SerializeField, Min(0f)] private float movementRange;
 
 
+        private Transform BodyTransform => body.transform;
         private float DeltaTime => m_IgnoreTimeScale ? Time.unscaledDeltaTime : Time.deltaTime;
         
         
@@ -34,15 +35,16 @@ namespace SpaceShipSystem
         private void OnPerformMove(SpaceShip.EventResponse response)
         {
             var direction = response.movementDirection;
+            var movement = Vector3.right * (direction * speed * DeltaTime);
+            var newPosition = BodyTransform.position + movement;
+            ValidatePosition(ref newPosition);
 
-            body.transform.position += Vector3.right * (direction * speed * DeltaTime);
-            ValidatePosition();
+            BodyTransform.position = newPosition;
         }
 
         private void OnCancelMove(SpaceShip.EventResponse response)
         {
             body.velocity = Vector3.zero;
-            ValidatePosition();
         }
 
         private void OnGamePaused(Game.EventResponse response)
@@ -56,21 +58,17 @@ namespace SpaceShipSystem
         }
 
 
-        private void ValidatePosition()
+        private void ValidatePosition(ref Vector3 position)
         {
-            var position = body.position;
-
             if (position.x > movementRange)
             {
                 position.x = movementRange;
-                body.transform.position = position;
                 return;
             }
 
             if (position.x < -movementRange)
             {
                 position.x = -movementRange;
-                body.transform.position = position;
             }
         }
 
