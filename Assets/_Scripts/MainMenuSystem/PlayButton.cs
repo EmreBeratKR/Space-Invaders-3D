f4b79f5979aba0d5a41faaf05b61b316176353e3
Emre Berat Kıran <87636497+EmreBeratKR@users.noreply.github.com>
2022-12-03 06:41:01 +0300

    Hot fix

--- Assets/_Scripts/MainMenuSystem/PlayButton.cs
+++ Assets/_Scripts/MainMenuSystem/PlayButton.cs
@@ -1,11 +1,46 @@
+using UnityEngine;
+
 namespace MainMenuSystem
 {
     public class PlayButton : MeshButton
     {
+        private void Awake()
+        {
+            AddListeners();
+        }
+
+        private void Start()
+        {
+            Show();
+        }
+        
+        private void OnDestroy()
+        {
+            RemoveListeners();
+        }
+
+
+        private void OnMainMenuLoaded(MainMenu.EventResponse response)
+        {
+            transform.localScale = Vector3.one;
+        }
+        
+
         protected override void OnClicked()
         {
             base.OnClicked();
             Game.Load();
+        }
+
+
+        private void AddListeners()
+        {
+            MainMenu.OnLoaded += OnMainMenuLoaded;
+        }
+
+        private void RemoveListeners()
+        {
+            MainMenu.OnLoaded -= OnMainMenuLoaded;   
         }
     }
 }
